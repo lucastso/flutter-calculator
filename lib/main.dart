9ddcea291054d7dd67a798a0b66ed6078668353d
Lucas Tassi Souza <lucastassisouza@gmail.com>
2023-04-11 12:54:26 -0300

    feat: handler dos botões e switch / app concluido

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _resValue = "0";
+  late final TextEditingController _textResultValueController;
+  String _sign = "";
+  String _firstNumber = "";
+  String _secondNumber = "";
 
-  void _handleButtonClick(String buttonValue) {
-    debugPrint(buttonValue);
+  @override
+  void initState() {
+    _textResultValueController = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textResultValueController.dispose();
+
+    super.dispose();
+  }
+
+  void _handleButtonClick(String pressedValue) {
+    setState(() {
+      if (pressedValue == "C") {
+        _textResultValueController.text = "0";
+        _sign = "";
+        _firstNumber = "";
+        _secondNumber = "";
+      }
+
+      String removeZero = _textResultValueController.text.replaceAll("0", "");
+      _textResultValueController.text = removeZero.toString();
+
+      switch (pressedValue) {
+        case "<":
+          var text = _textResultValueController.text.split("");
+          text.removeLast();
+          _textResultValueController.text = text.join();
+          break;
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          _textResultValueController.text += pressedValue;
+          break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+          _sign = pressedValue;
+          _firstNumber = _textResultValueController.text;
+          _textResultValueController.text = "0";
+          break;
+        case ".":
+          if (_textResultValueController.text.split("").contains(".")) {
+            return;
+          } else {
+            _textResultValueController.text += pressedValue;
+          }
+          break;
+        case "=":
+          _secondNumber = _textResultValueController.text;
+          if (_sign == "+") {
+            var result =
+                double.parse(_firstNumber) + double.parse(_secondNumber);
+            _textResultValueController.text = result.toString();
+          } else if (_sign == "-") {
+            var result =
+                double.parse(_firstNumber) - double.parse(_secondNumber);
+            _textResultValueController.text = result.toString();
+          } else if (_sign == "*") {
+            var result =
+                double.parse(_firstNumber) * double.parse(_secondNumber);
+            _textResultValueController.text = result.toString();
+          } else if (_sign == "/") {
+            if (_secondNumber == "0") {
+              _textResultValueController.text = "Err";
+              return;
+            } else {
+              var result =
+                  double.parse(_firstNumber) / double.parse(_secondNumber);
+              _textResultValueController.text = result.toString();
+            }
+          }
+          break;
+        default:
+          return;
+      }
+    });
   }
 
   @override
@@ -86,12 +174,17 @@ class _MyHomePageState extends State<MyHomePage> {
                               border: Border.all(
                                   color: Colors.blue.shade800, width: 6),
                             ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                              child: Text(
-                                "00",
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, right: 8.0),
+                              child: TextField(
+                                controller: _textResultValueController,
                                 textAlign: TextAlign.end,
-                                style: TextStyle(
+                                decoration: const InputDecoration(
+                                    hintText: "0",
+                                    hintStyle: TextStyle(
+                                        fontSize: 40, color: Colors.blueGrey)),
+                                style: const TextStyle(
                                     fontSize: 40, color: Colors.blueGrey),
                               ),
                             ),
@@ -177,7 +270,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     MaterialButton(
                       onPressed: () {
-                        _handleButtonClick("");
+                        _handleButtonClick("<");
                       },
                       padding: EdgeInsets.zero,
                       child: SizedBox(
@@ -409,7 +502,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     MaterialButton(
                       onPressed: () {
-                        _handleButtonClick("x");
+                        _handleButtonClick("*");
                       },
                       padding: EdgeInsets.zero,
                       child: SizedBox(
@@ -425,7 +518,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 borderRadius: BorderRadius.circular(10)),
                             child: const Center(
                               child: Text(
-                                "x",
+                                "*",
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
                                     fontSize: 32, color: Colors.blueGrey),
